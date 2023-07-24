d56a2698e1449532a4a76a0b659a580c3c35373d
Temitope Enikankiselu <ask4teymitee@gmail.com>
2023-07-24 02:21:57 +0100

    little refactoring

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -7,50 +7,63 @@ import {
   LightModeOutlined,
   DarkModeOutlined,
 } from "@mui/icons-material";
-import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  Divider,
+  IconButton,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import Flexbetween from "components/Flexbetween";
 import React from "react";
 
 const Navbar = ({ mode, setMode }) => {
+  const isMobile = useMediaQuery("(max-width:600px)");
   const theme = useTheme();
   const neutral = theme.palette.neutral.main;
+
   return (
-    <Box padding={"1rem 6%"}>
-      <Flexbetween mb={"0.5rem"}>
-        <Flexbetween gap={"0.5rem"}>
-          <IconButton>
-            <EmailOutlined />
-          </IconButton>
-          <Typography variant="p" color={neutral} p={"8px 0"}>
-            topeenikankiselu@gmail.com
-          </Typography>
-        </Flexbetween>
-        <Flexbetween gap={"1rem"}>
-          <IconButton>
-            <LinkedIn />
-          </IconButton>
-          <IconButton>
-            <Twitter />
-          </IconButton>
-          <IconButton>
-            <Instagram />
-          </IconButton>
-          <IconButton>
-            <GitHub />
-          </IconButton>
-          {mode === "dark" ? (
-            <IconButton onClick={() => setMode("light")}>
-              <LightModeOutlined />
-            </IconButton>
-          ) : (
-            <IconButton onClick={() => setMode("dark")}>
-              <DarkModeOutlined />
-            </IconButton>
-          )}
-        </Flexbetween>
-      </Flexbetween>
-      <Divider />
-    </Box>
+    <>
+      {!isMobile && (
+        <Box padding={"1rem 6%"}>
+          <Flexbetween mb={"0.5rem"}>
+            <Flexbetween gap={"0.5rem"}>
+              <IconButton>
+                <EmailOutlined />
+              </IconButton>
+              <Typography variant="p" color={neutral} p={"8px 0"}>
+                topeenikankiselu@gmail.com
+              </Typography>
+            </Flexbetween>
+            <Flexbetween gap={"1rem"}>
+              <IconButton>
+                <LinkedIn />
+              </IconButton>
+              <IconButton>
+                <Twitter />
+              </IconButton>
+              <IconButton>
+                <Instagram />
+              </IconButton>
+              <IconButton>
+                <GitHub />
+              </IconButton>
+              {mode === "dark" ? (
+                <IconButton onClick={() => setMode("light")}>
+                  <LightModeOutlined />
+                </IconButton>
+              ) : (
+                <IconButton onClick={() => setMode("dark")}>
+                  <DarkModeOutlined />
+                </IconButton>
+              )}
+            </Flexbetween>
+          </Flexbetween>
+          <Divider />
+        </Box>
+      )}
+    </>
   );
 };
 
